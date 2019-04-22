76c9a0911efcbf3421f7e8563351edfaf778a280
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-04-22 21:17:50 +0900

    change assembler gas to nasm

--- experiment/x64_boot/ipl.s
+++ experiment/x64_boot/ipl.s
@@ -1,30 +1,28 @@
-.intel_syntax noprefix
-.text
-.code16
+BITS 16
 
-#jmp to IPL(generary : EB 3E)
+;jmp to IPL(generary : EB 3E)
 jmp entry
-.byte   0x90
-.ascii "IPL     " # name string of OEM (8byte)
-.word   512 # sector size(should be 512 byte)
-.byte   1 # clustor size (should be 1 sector)
-.word   1 # start sector of FAT(normally 1 sector)
-.byte   2 # number of FAT (should be 2)
-.word   224 # size of root directory (normally 224)
-.word   2880 # size of drive(should be2880 sector)
-.byte   0xf0 # media type(should be 0xf0)
-.word   9 # length of FAT area(should be 9 sector)
-.word   18 # number of sector per track (should be 18 sector)
-.word   2 # number of head(should be 2)
-.int    0 # partion(should be 0 if not use partion)
-.int    2880 # size of dirve
-.byte   0,0,0x29 # unknown(physical drive number, reserved, sigunature(if set 0x29, following 3 data is used)
-.int    0xffffffff # maybe serial number ofvolume
-.ascii  "UROBOROS-OS" # disk name(11byte)
-.ascii  "FAT12   " # name of format(8byte)
-.skip   18,0 # padding?
+DB   0x90
+DB "IPL     " ; name string of OEM (8byte)
+DW   512 ; sector size(should be 512 byte)
+DB   1 ; clustor size (should be 1 sector)
+DW   1 ; start sector of FAT(normally 1 sector)
+DB   2 ; number of FAT (should be 2)
+DW   224 ; size of root directory (normally 224)
+DW   2880 ; size of drive(should be2880 sector)
+DB   0xf0 ; media type(should be 0xf0)
+DW   9 ; length of FAT area(should be 9 sector)
+DW   18 ; number of sector per track (should be 18 sector)
+DW   2 ; number of head(should be 2)
+DD    0 ; partion(should be 0 if not use partion)
+DD    2880 ; size of dirve
+DB   0,0,0x29 ; unknown(physical drive number, reserved, sigunature(if set 0x29, following 3 data is used)
+DD    0xffffffff ; maybe serial number ofvolume
+DB  "UROBOROS-OS" ; disk name(11byte)
+DB  "FAT12   " ; name of format(8byte)
+TIMES 18 DB 0
 
-# 62 byte地点
+; 62 byte地点
 entry:
     mov ax, 0
     mov ss, ax
@@ -32,7 +30,7 @@ entry:
     mov ds, ax
     mov es, ax
     
-    mov si, offset msg
+    mov si, msg
 
 putloop:
     mov al, [si]
@@ -50,9 +48,9 @@ fin:
     jmp fin
 
 msg:
-    .ascii "\nhello, world\n\n"
+    DB `\nhello, world\n\n\0`
 
-#end of boot sector(must be 0x55, 0xaa)
-.org 0x01fe
-.byte 0x55, 0xaa
+;end of boot sector(must be 0x55, 0xaa)
+TIMES 0x01fe-($-$$) DB 0
+DB 0x55, 0xaa
 
